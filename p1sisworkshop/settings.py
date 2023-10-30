76de953fdd3f92f62b0ddd8c87bd23c7ffd032c3
Juan181803 <jframirez.valencia@gmail.com>
2023-10-30 16:14:02 -0500

    correccion

--- p1sisworkshop/settings.py
+++ p1sisworkshop/settings.py
@@ -122,7 +122,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'static'
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 STATIC_URL = 'static/'
 LOGIN_URL = '/signin'
